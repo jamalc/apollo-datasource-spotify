eb08157e8e5226a2588dc19c36c990b281f113a7
jamal carvalho <jamal.a.carvalho@gmail.com>
2021-05-29 14:08:52 -0400

    remove apollo server dependency

--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,6 +1,4 @@
-import { gql } from 'apollo-server';
-
-export const typeDefs = gql`
+export const typeDefs = `
   type AlbumObject {
     """
     The type of the album: album, single, or compilation.
